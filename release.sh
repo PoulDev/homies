a22096d2b1df24959223bb4272aa2c5502f82077
PoulDev <filippotrabattoni@gmail.com>
2025-05-03 17:51:04 +0200

    Useless python files

---
+++ release.sh
@@ -0,0 +1,14 @@
+
+lastTag=$(git tag --sort=-v:refname | head -n 1)
+echo "Enter tag, last tag: $lastTag"
+read tag
+
+echo "Enter commit message:"
+read commitmsg
+
+git add .
+git tag $tag
+git commit -m "$commitmsg"
+git push origin $tag
+git push origin main
+
