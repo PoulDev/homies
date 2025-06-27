95cae97871cc502f378cfa8a9a492d77a57c9eba
PoulDev <83310250+PoulDev@users.noreply.github.com>
2025-06-27 14:19:43 +0200

    better error messages

--- main.go
+++ main.go
@@ -16,19 +16,19 @@ func main() {
 	log.Println(avatar.RandAvatar());
 	err := config.LoadConfig();
 	if (err != nil) {
-		log.Println("!! FROCIAZZO MANCANO LE ENVIRONMENT VARIABLES");
-		log.Panic(err);
+		log.Println("Missing environment variables!")
+		log.Fatal(err)
 	}
 
 	err = db.ConnectDatabase()
 	if (err != nil) {
-		log.Println("!! CONNESSIONE AL DATABASE FALLITA");
-		log.Panic(err);
+		log.Println("Connection to the database failed!")
+		log.Panic(err)
 	}
 
 	router := gin.Default()
 
-	routes.SetupRoutes(router);
+	routes.SetupRoutes(router)
 	
 	err = router.Run(fmt.Sprintf(":%d", config.HostPort))
 	if (err != nil) {
